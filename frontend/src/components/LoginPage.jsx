bb2aa3c14aa57fb3f219ab50850b9047e42efba3
Denis Karnafel <denisk9999@mail.ru>
2024-03-29 00:35:40 +0700

    update LoginPage.jsx

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,17 +1,42 @@
-import { NavLink } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate, NavLink } from 'react-router-dom';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
 import img from '../assets/avatar.jpg';
 import getPath from '../routes.js';
 
 const LoginPage = () => {
+  const [loginFailed, setFailedLogin] = useState(false);
+  const inputEl = useRef();
+  const navigate = useNavigate();
+  useEffect(() => {
+    inputEl.current.focus();
+  }, [loginFailed]);
   const formik = useFormik({
     initialValues: {
       username: '',
       password: '',
     },
-    onSubmit: (values) => {
-      console.log(values);
-      return values;
+    validationSchema: yup.object({
+      username: yup.string().required(),
+      password: yup.string().required(),
+    }),
+    onSubmit: async (values) => {
+      setFailedLogin(false);
+      try {
+        const result = await axios.post(getPath.loginPath(), values);
+        localStorage.setItem('userToken', result.data.token);
+        navigate(getPath.chatPage());
+      } catch (error) {
+        formik.setSubmitting(false);
+        if (error.response.status === 401) {
+          setFailedLogin(true);
+          inputEl.current.select();
+        } else {
+          throw error;
+        }
+      }
     },
   });
 
@@ -31,13 +56,15 @@ const LoginPage = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.username}
-                    className="form-control"
+                    className={loginFailed ? 'form-control is-invalid' : 'form-control'}
                     type="text"
                     name="username"
                     autoComplete="username"
                     placeholder="Ваш ник"
                     id="username"
                     required
+                    disabled={formik.isSubmitting}
+                    ref={inputEl}
                   />
                   <label htmlFor="username">Ваш ник</label>
                 </div>
@@ -46,17 +73,19 @@ const LoginPage = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.password}
-                    className="form-control"
+                    className={loginFailed ? 'form-control is-invalid' : 'form-control'}
                     type="password"
                     name="password"
                     autoComplete="password"
                     placeholder="Пароль"
                     id="password"
                     required
+                    disabled={formik.isSubmitting}
                   />
                   <label htmlFor="password">Пароль</label>
+                  {loginFailed ? <div className="invalid-tooltip">Неверные имя пользователя или пароль</div> : ''}
                 </div>
-                <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+                <button disabled={formik.isSubmitting} type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
               </form>
             </div>
             <div className="card-footer p-4">
