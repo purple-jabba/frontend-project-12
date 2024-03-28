279ec590140872e4937133ba85a473f9ec981b79
Denis Karnafel <denisk9999@mail.ru>
2024-03-29 00:35:56 +0700

    update App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import getPath from '../routes.js';
 
 import NotFound from './NotFoundPage.jsx';
 import LoginPage from './LoginPage.jsx';
+import CheckTokenPage from './CheckTokenPage.jsx';
 
 const App = () => (
   <div className="h-100">
     <div className="d-flex flex-column h-100">
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path={getPath.chatPage()} element={<CheckTokenPage />} />
+          <Route path={getPath.loginPage()} element={<LoginPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
