44ad096e9564d835802d035bcfcb28ff89831c3b
Denis Karnafel <denisk9999@mail.ru>
2024-04-01 01:35:24 +0700

    update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,8 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
+  messagesPath: () => [apiPath, 'messages'].join('/'),
+  channelsPath: () => [apiPath, 'channels'].join('/'),
   loginPage: () => '/login',
   chatPage: () => '/',
   notFoundPage: () => '*',
