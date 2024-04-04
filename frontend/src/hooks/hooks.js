c0a0d631e4e2a51075bd437fe05a2c402841a88f
Denis Karnafel <denisk9999@mail.ru>
2024-04-04 23:26:10 +0700

    update hooks.js

--- frontend/src/hooks/hooks.js
+++ frontend/src/hooks/hooks.js
@@ -3,3 +3,5 @@ import { useSelector } from 'react-redux';
 export const useAuth = () => useSelector((state) => state.auth);
 
 export const useSelectedChannel = () => useSelector((state) => state.selectedChannel);
+
+export const useModal = () => useSelector((state) => state.modal);
