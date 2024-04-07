ff6854b6aa35a5989270d8a920612af3906f5b5b
Denis Karnafel <denisk9999@mail.ru>
2024-04-07 18:37:44 +0700

    update hooks.js

--- frontend/src/hooks/hooks.js
+++ frontend/src/hooks/hooks.js
@@ -5,3 +5,7 @@ export const useAuth = () => useSelector((state) => state.auth);
 export const useSelectedChannel = () => useSelector((state) => state.selectedChannel);
 
 export const useModal = () => useSelector((state) => state.modal);
+
+export const useMessages = () => useSelector((state) => state.messages);
+
+export const useChannels = () => useSelector((state) => state.channels);
