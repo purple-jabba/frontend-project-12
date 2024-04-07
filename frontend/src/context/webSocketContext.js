d80503d80719bf6ef0fa19274205db989ee1f319
Denis Karnafel <denisk9999@mail.ru>
2024-04-08 01:07:10 +0700

    update webSocketContext.js

--- frontend/src/context/webSocketContext.js
+++ frontend/src/context/webSocketContext.js
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 import store from '../slices/index.js';
 import { addNewMesage } from '../slices/messagesSlice';
 import { addNewChannel, deleteChannel, renameChannel } from '../slices/channelsSlice.js';
+import { selectCurrentChannel, selectDefaultChannel } from '../slices/selectChannelSlice.js';
 
 export const socket = io();
 
@@ -26,10 +27,18 @@ socket.on('newChannel', (payload) => {
 
 socket.on('removeChannel', (payload) => {
   console.log(payload);
+  const { selectedChannel } = store.getState();
+  if (selectedChannel.currentChannelId.toString() === payload.id) {
+    dispatch(selectDefaultChannel());
+  }
   dispatch(deleteChannel(payload));
 });
 
 socket.on('renameChannel', (payload) => {
   console.log(payload);
+  const { selectedChannel } = store.getState();
+  if (selectedChannel.currentChannelId.toString() === payload.id) {
+    dispatch(selectCurrentChannel({ id: selectedChannel.currentChannelId, name: payload.name }));
+  }
   dispatch(renameChannel(payload));
 });
